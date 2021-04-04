3f21f34eb3e6d1cfd3469983d057a0b10b735b84
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-04-04 12:22:43 +0100

    Ajout d'un cadre non fonctionelle DataChat

---
+++ src/components/DataChat.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+function DataChat() {
+    
+    const [startAvailable, setStart] = useState(true)
+    const [sendAvailable, setSend] = useState(false)
+    const [hangupAvailable, setHangup] = useState(false)
+    const classes = useStyles();
+
+    return (
+        <div>
+            <form className={classes.root} noValidate autoComplete="off">
+                
+                <TextField id="standard-basic" label="Identifiant" />
+                <TextField id="standard-basic" label="Destinataire" />
+                <Button onClick={start} disabled={!startAvailable}>
+                    Démarrer
+                </Button>
+
+                <TextField id="standard-basic" label="Message" />
+                <Button onClick={send} disabled={!callAvailable}>
+                    Envoyer
+                </Button>
+                
+                <div>
+
+                </div>
+
+                <Button onClick={hangUp} disabled={!hangupAvailable}>
+                    Hang Up
+                </Button>
+            </form>
+        </div>
+    )
+}
+
+export default DataChat
