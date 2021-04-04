669c94445655fb769b5bdca62d64f552417bc3b6
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-04-04 10:14:56 +0100

    Organisation pour entamer le TP3 avec le clonage du tp1

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html", 
+  filename: "./index.html"
+});
+module.exports = (env, argv) => {
+  console.log(argv.mode);
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "[name].js"
+    },
+    plugins: [htmlPlugin],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [ "babel-loader" , "eslint-loader" ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          loader: "file-loader",
+          options: { name: '/img/[name].[ext]' }
+        }
+      ]
+    }
+  };
+};
