3cd89023de5b399f11a2f5b3b28cd666b5fbbdfa
Papa Ndiaga SARR <ndiagasarr93@gmail.com>
2021-04-05 05:09:44 +0100

    FIX problème de peer

--- src/components/DataChat.jsx
+++ src/components/DataChat.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button } from '@material-ui/core';
+import { Peer } from 'peer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,11 +12,51 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+let peer = null;
+let connexion = null;
+
+const initpeer = (localID) => {
+  console.log('start');
+
+  peer = new Peer(localID, {
+    host: 'localhost',
+    port: 3000,
+    path: '/myapp',
+  });
+
+  peer.on('open', (id) => {
+    console.log('Mon identifiant est: ', id);
+  });
+};
+
+const initconn = function initconn(id) {
+  connexion = peer.connect(id);
+};
+
 function DataChat() {
   const [startAvailable, setStart] = useState(true);
   const [sendAvailable, setSend] = useState(false);
   const [hangupAvailable, setHangup] = useState(false);
+  const [callAvailable, setCall] = useState(false);
+
   const classes = useStyles();
+
+  const onStart = function Start(id) {
+    setCall(true);
+    setStart(false);
+    initpeer(id);
+  };
+
+  const onCall = function Call(id) {
+    setSend(true);
+    setHangup(true);
+    setCall(false);
+    initconn(id);
+  };
+
+  const onSend = function Send(message) {
+    connexion.send(message);
+  };
 
   return (
     <div>
@@ -23,18 +64,18 @@ function DataChat() {
 
         <TextField id="standard-basic" label="Identifiant" />
         <TextField id="standard-basic" label="Destinataire" />
-        <Button onClick={start} disabled={!startAvailable}>
+        <Button onClick={onStart} disabled={!startAvailable}>
           Démarrer
         </Button>
 
         <TextField id="standard-basic" label="Message" />
-        <Button onClick={send} disabled={!callAvailable}>
+        <Button onClick={onSend} disabled={!callAvailable}>
           Envoyer
         </Button>
 
         <div />
 
-        <Button onClick={hangUp} disabled={!hangupAvailable}>
+        <Button onClick={onCall} disabled={!hangupAvailable}>
           Hang Up
         </Button>
       </form>
